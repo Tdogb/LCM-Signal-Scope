37eb53c3f22ce50cf08ffaa6bf94fe17b1750e72
Tanner <tdogbeard@gmail.com>
2019-12-13 17:46:14 -0500

    update

--- signal-scope.py
+++ signal-scope.py
@@ -30,15 +30,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from numpy import fft
+import scipy.fftpack
 
 fig, axes = plt.subplots(len(lcm_channels),1)
 x = [[],[],[],[]]
 y = [[],[],[],[]]
+yo = [[]]
 #y2 = []
 loop_time = 0
 
 lcm_ = LCM()
-
+start = 0
+end = 0
+delta = 0.01
 
 def initPlots():
     index = 0
@@ -49,28 +53,47 @@ def initPlots():
         index += 1
 
 def runPlots(i):
-    global loop_time, x, y, lcm_channels, lcm_, currentValues
+    start = time.time()
+    global loop_time, delta, x, y, y2, lcm_channels, lcm_, currentValues
     index = 0
     for ax in axes:
-        x[index].append(loop_time)
-        y[index].append(currentValues[index])
-        #y2.append(currnetValue1)
-        if len(x[index]) == sample_size:
-            x[index].pop(0)
-            y[index].pop(0)
+        if index == 0:
+            yo[index].append(currentValues[index])
+            if len(x[index]) == 50:
+                # x[index].pop(0)
+                yo[index].pop(0)
+            # Number of samplepoints
+            N = len(yo[index])
+            # sample spacing
+            T = 1.0 / 50.0
+            # x2 = np.linspace(0.0, 1.0, N/2)
+            # yv = np.sin(50.0 * 2.0*np.pi*x2) + 0.5*np.sin(80.0 * 2.0*np.pi*x2)
+            if len(yo[index]) > 10:
+                y3 = scipy.fftpack.fft(yo[index])
+                y3[:] = [xa * delta for xa in y3]
+                # x2 = np.linspace(0.0,100,len(yo[index]))
+                x2 = np.arange(0,len(yo[index])/2, step=1)
+                y2 = 2.0/N * np.abs(y3[:N//2])
+                axes[index].clear()
+                axes[index].set_ylim(0,2)
+                axes[index].plot(x2,y2)
+            # print(y[index])
+        else:
+            # x[index].append(loop_time)
+            # y[index].append(currentValues[index])
             #y2.pop(0)
         # x[index] = x[-sample_size:]
         # y[index] = y[-sample_size:]
-        axes[index].clear()
-        axes[index].plot(x[index],y[index])
+            axes[index].clear()
+            axes[index].plot(x[index],y[index])
         #axes[index].plot(x[index],y2)
         index += 1
     loop_time += 1
     # if loop_time % 30:
     lcm_.handle()
+    delta = end-start
+    # print(delta)
 
-def getLcmData(index):
-    return currentValues[index] #randint(0,10)
 
 if __name__ == "__main__":
     initPlots()
